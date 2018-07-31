13a7c2a589376b5ec3e067333eead167c4fb3150
JP <jpangelle15@gmail.com>
2018-07-31 00:07:23 -0500

    Stand up Twitter bot
    	-install twitter api package
    	-set up twitter api env variables
    	-write function to compose and post tweet
    	-write setInterval to fetch, sort data from MongoDB then invoke composeTweet every hour

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cron from 'node-cron';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
 
+import Sped from './models/sped';
+import composeTweet from './twitter-api';
 import launchBrowser from './fast-api/api';
 
 config();
@@ -18,6 +20,22 @@ mongoose.connection
 cron.schedule('*/10 * * * *', launchBrowser, null, true, 'America/Chicago');
 
 cron.schedule('00 */1 * * *', () => { console.log('need to run stats here', new Date().toISOString()); }, null, true, 'America/Chicago');
+
+setInterval(() => {
+  Sped.find({}).sort({ timestamp: -1 }).exec((err, res) => {
+    if (res) {
+      const { speed, timestamp } = res[0];
+      if (speed < (1000 * 0.75)) {
+        composeTweet({
+          speed,
+          timestamp,
+        });
+      }
+    } else {
+      console.log('Error fetching data:', err);
+    }
+  });
+}, 60 * 60 * 1000);
 
 app.get('/', async (req, res) => {
   launchBrowser(res);
