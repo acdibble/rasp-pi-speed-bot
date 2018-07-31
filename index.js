c59299077ab75923dead50d094e684d061d90e45
acdibble <acdibble@gmail.com>
2018-07-30 21:50:00 -0500

    Fetch speed with headless browser

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import puppeteer from 'puppeteer';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Howdy world.');
+const speeds = [];
+
+const checkForSucceeded = async (res, browser, page) => {
+  const bodyHandle = await page.$('#speed-value.succeeded');
+  if (bodyHandle) {
+    const speed = await page.evaluate(body => body.innerHTML, bodyHandle);
+    await browser.close();
+
+    if (res) {
+      return res.send({ timestamp: new Date().toISOString(), speed });
+    }
+
+    return speeds.push({ timestamp: new Date().toISOString(), speed });
+  }
+
+  return setTimeout(checkForSucceeded, 5000, res, browser, page);
+};
+
+const launchBrowser = async (res) => {
+  await puppeteer.launch().then(async (browser) => {
+    const page = await browser.newPage();
+    await page.goto('https://fast.com/');
+    checkForSucceeded(res, browser, page);
+  });
+};
+
+app.get('/', async (req, res) => {
+  launchBrowser(res);
 });
 
 app.listen(3000, () => console.log('Listening on 3000.'));
