052f2ee0fcc7c537b85120c2e9875018122e5253
acdibble <acdibble@gmail.com>
2018-07-31 21:58:33 -0500

    Clean up fast api

--- src/fast-api/api.js
+++ src/fast-api/api.js
@@ -1,30 +1,26 @@
 import puppeteer from 'puppeteer';
 import { Sped } from '../models';
 
-const checkForSucceeded = async (res, browser, page) => {
+const checkForSucceeded = async (browser, page) => {
   const bodyHandle = await page.$('#speed-value.succeeded');
   if (bodyHandle) {
     const speed = await page.evaluate(body => body.innerHTML, bodyHandle);
     await browser.close();
 
-    if (res) {
-      return res.send({ timestamp: new Date().toISOString(), speed });
-    }
-
     return Sped.create({ speed: +speed, timestamp: new Date() });
   }
 
-  return setTimeout(checkForSucceeded, 5000, res, browser, page);
+  return setTimeout(checkForSucceeded, 5000, browser, page);
 };
 
-const launchBrowser = async (res) => {
+const launchBrowser = async () => {
   const opts = process.env.NODE_ENV === 'production'
     ? { args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu'], executablePath: '/usr/bin/chromium-browser' }
     : {};
   const browser = await puppeteer.launch(opts);
   const page = await browser.newPage();
   await page.goto('https://fast.com/');
-  checkForSucceeded(res, browser, page, Sped);
+  checkForSucceeded(browser, page);
 };
 
 export default launchBrowser;
