9b8f417911ac5aad92026f63bcdb62827369b86f
acdibble <acdibble@gmail.com>
2018-08-02 00:17:07 -0500

    Add more stats

--- src/statistics/stats.js
+++ src/statistics/stats.js
@@ -10,12 +10,14 @@ const getStatsForPast = time => new Promise(async (resolve, reject) => {
     reject(e);
   }
 
-  const speeds = speds.map(({ speed }) => speed);
+  const speeds = speds.map(({ speed }) => speed).sort((a, b) => a - b);
 
   const stats = {
     mean: mean(speeds),
     std: std(speeds),
+    min: speeds[0],
     median: median(speeds),
+    max: speeds[speeds.length - 1],
     sampleSize: speeds.length,
     timestamp: new Date(),
   };
