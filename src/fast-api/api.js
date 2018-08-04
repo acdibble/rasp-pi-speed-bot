dddfdfa95eef72ff4a4e0c42d86e1af62a098fe6
acdibble <acdibble@gmail.com>
2018-08-04 16:52:37 -0500

    Revert "Replace fast with speedtest and update stats and schema"
    This reverts commit 5ee972dc16512b031ed160d4ad06061fdb72f8a1.

---
+++ src/fast-api/api.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+import { Sped } from '../models';
+
+const checkForSucceeded = async (browser, page) => {
+  const elHandle = await page.$('#speed-value.succeeded');
+  if (elHandle) {
+    const speed = await page.evaluate(el => el.innerHTML, elHandle);
+    await browser.close();
+
+    return Sped.create({ speed: +speed, timestamp: new Date() });
+  }
+
+  return setTimeout(checkForSucceeded, 5000, browser, page);
+};
+
+const launchBrowser = async () => {
+  const opts = process.env.NODE_ENV === 'production'
+    ? { args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu'], executablePath: '/usr/bin/chromium-browser' }
+    : {};
+  const browser = await puppeteer.launch(opts);
+  const page = await browser.newPage();
+  await page.goto('https://fast.com/');
+  checkForSucceeded(browser, page);
+};
+
+export default launchBrowser;
