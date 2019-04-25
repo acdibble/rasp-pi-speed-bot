a61a2851c43200c2ca02eaf4dcda2b3632338a13
Andrew Dibble <acdibble@gmail.com>
2019-04-25 18:56:41 -0500

    Fix tweets for Twitter terms of use

--- test/twitter-api/tweets.test.js
+++ test/twitter-api/tweets.test.js
@@ -10,12 +10,12 @@ describe('Tweets', () => {
   it('should return tweet for past hour', () => {
     const tweet = 'pastHour';
     const text = tweets[tweet](tweetParams);
-    assert.equal(text.status, 'Calculated our average speed from the past hour (6 samples): 100.0mbps, but we are paying for 1000mbps. @ATTCares #ATTDoesNotCare #ATTCaresAboutMoney');
+    assert.equal(text.status, 'Calculated our average speed from the past hour (6 samples): 100.0mbps, but we are paying for 1000mbps. #ATTDoesNotCare #ATTCaresAboutMoney');
   });
 
   it('should return tweet for past day', () => {
     const tweet = 'pastDay';
     const text = tweets[tweet](tweetParams);
-    assert.equal(text.status, 'Calculated our average speed from the past day (6 samples): only 100.0mbps. Sad! @ATTCares #MakeATTCareAgain');
+    assert.equal(text.status, 'Calculated our average speed from the past day (6 samples): only 100.0mbps. Sad! #MakeATTCareAgain');
   });
 });
